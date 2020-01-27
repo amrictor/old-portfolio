c495f8f11db3e26bcb022986c91980b5374f0015
Abby Rictor <amrictor@gmail.com>
2020-01-27 15:25:23 -0700

    Updates

---
+++ precache-manifest.e1605f98ee632bc65ff26a0e3e7686de.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0335866d96b79095db2c9f3f521187d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2872008ded9f4765201a",
+    "url": "/static/css/2.0996cbbd.chunk.css"
+  },
+  {
+    "revision": "bd18469de7164ba0bcf1",
+    "url": "/static/css/main.b276eb63.chunk.css"
+  },
+  {
+    "revision": "2872008ded9f4765201a",
+    "url": "/static/js/2.6f46b32b.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.6f46b32b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd18469de7164ba0bcf1",
+    "url": "/static/js/main.67a504ff.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "45f60eb72cc25544fef4dcf105705885",
+    "url": "/static/media/CS464FinalReport.45f60eb7.pdf"
+  },
+  {
+    "revision": "730aaf92b4796bf7886a405f0a603d3d",
+    "url": "/static/media/Resume.730aaf92.pdf"
+  },
+  {
+    "revision": "7e310f290b3c8ea07e0bf0d44f9974e9",
+    "url": "/static/media/acmw.7e310f29.png"
+  },
+  {
+    "revision": "a2bd0a1fa6d6e0b7c709463cf688d4f4",
+    "url": "/static/media/anim1024.a2bd0a1f.gif"
+  },
+  {
+    "revision": "a07c6e1d29a7748e92ff0fcab6207d17",
+    "url": "/static/media/brushonoff.a07c6e1d.gif"
+  },
+  {
+    "revision": "86c8ce119d6a958331c4fcc5d7df8cc1",
+    "url": "/static/media/brushsize.86c8ce11.gif"
+  },
+  {
+    "revision": "0996c2f8c26b45d5847207ab0b21a61c",
+    "url": "/static/media/carving.0996c2f8.gif"
+  },
+  {
+    "revision": "5acd7a483d8cb067d19c170f9958b563",
+    "url": "/static/media/congogameplay.5acd7a48.gif"
+  },
+  {
+    "revision": "501fbe8316f51af79a4e9fb035025827",
+    "url": "/static/media/cs[u].501fbe83.png"
+  },
+  {
+    "revision": "ec04d739a459a45481473e666dd235ae",
+    "url": "/static/media/csu.ec04d739.svg"
+  },
+  {
+    "revision": "223ad4535a2bdaf95c380d7b07fb0ba0",
+    "url": "/static/media/extralife.223ad453.png"
+  },
+  {
+    "revision": "78a5630f7a46c1c3c75e1426c010a0a0",
+    "url": "/static/media/eyh.78a5630f.png"
+  },
+  {
+    "revision": "89794f9a5c1e2b2200a2f84d4dddce04",
+    "url": "/static/media/game.89794f9a.gif"
+  },
+  {
+    "revision": "d3a6e0dd578db7f99e8b6e891f588119",
+    "url": "/static/media/hpe.d3a6e0dd.svg"
+  },
+  {
+    "revision": "82866b8ebbf39f37148e42e2303de434",
+    "url": "/static/media/lat-lon.82866b8e.png"
+  },
+  {
+    "revision": "aa01a929ba2b354fb6caf7f24276585d",
+    "url": "/static/media/lyrics.aa01a929.gif"
+  },
+  {
+    "revision": "63f00b945ad7c0824f3c304667c550f5",
+    "url": "/static/media/pelco.63f00b94.png"
+  },
+  {
+    "revision": "c29b24b9e005d2eb13d0f4de35b55604",
+    "url": "/static/media/poem1.c29b24b9.png"
+  },
+  {
+    "revision": "ec589eaff7f76a9307906dea09b63cc0",
+    "url": "/static/media/poem2.ec589eaf.png"
+  },
+  {
+    "revision": "e489dc2db4eea55864863791ef57028b",
+    "url": "/static/media/rotate.e489dc2d.gif"
+  },
+  {
+    "revision": "0b25f61e70c155c71de16681a1a751b8",
+    "url": "/static/media/trip_planner.0b25f61e.gif"
+  }
+]);
